7b01ad214e047ac768d0f8904f4256fd9112ec21
zlatkikz <kirannandakumar94@gmail.com>
2025-10-27 10:22:45 +0530

    splash screen updates

--- ui/src/components/SplashScreen.tsx
+++ ui/src/components/SplashScreen.tsx
@@ -2,7 +2,6 @@ import { useState, useEffect } from "react";
 import { useNavigate } from "@tanstack/react-router";
 import Logo from "../assets/wplff.svg";
 
-
 const SplashScreen = () => {
   const [showSplash, setShowSplash] = useState(true);
   const navigate = useNavigate();
@@ -20,17 +19,22 @@ const SplashScreen = () => {
     <>
       {showSplash && (
         <div className="flex items-center justify-center h-screen bg-gradient-to-r from-blue-500 to-purple-600">
-          <div className="text-center">
-            <div className="flex items-center gap-1">
-              <div className="w-10 h-10 rounded-sm flex items-center justify-center">
-                <img src={Logo} alt="PLogo" className="w-8 h-8" />
-              </div>
-              {/* Fantasy Text */}
-              <h1 className="text-white text-2xl font-bold tracking-tight">
-                WPL 
-              </h1>
+          <div className="flex items-center justify-center">
+            <div className="w-14 h-14 rounded-sm flex items-center justify-center">
+              <img
+                src={Logo}
+                alt="PLogo"
+                className="w-12 h-12 animate-bounce opacity-90"
+              />
             </div>
-             <h2 className="text-white text-lg font-bold tracking-tight">Fantasy Football</h2>
+            <div className="flex-row h-14">
+              <h1 className="text-white text-2xl font-bold tracking-tight animate-pulse">
+                WPL
+              </h1>
+              <h2 className="text-white text-lg font-bold tracking-tight animate-pulse">
+                Fantasy Football
+              </h2>
+            </div>
           </div>
         </div>
       )}
